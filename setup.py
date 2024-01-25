4436a5f7651037a749f0cb1d3cc424fa34a9081b
SachinSushilSingh <sachinsushilsingh@gmail.com>
2024-01-25 15:14:09 +0530

    3rd commit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import pathlib
 from setuptools import setup, find_packages
 
-HERE=pathlib.path(__file__).parent
+HERE=pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
     name='TOPSIS-Sachin-102103575',
     version='0.1',
-    description='It does a topsis analysis of a given csv file'
+    description='It does a topsis analysis of a given csv file',
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Liquidator420/Topsis-Sachin-102103575",
